cc668cac11de9609bf1cd398107d5f497331dd8f
BKONTECH Team <110591367+BKONTECH@users.noreply.github.com>
2022-08-12 06:56:55 +0700

    Move script source from main.html to func.js

---
+++ func.js
@@ -0,0 +1,9 @@
+
+function myFunction() {
+  var myWindow = window.open("", "_self");
+  myWindow.document.write("<p>I replaced the current window.</p>");
+}
+
+function myFunction2() {
+  window.open("https://www.bkontech.com", "_self");
+}
