37ab4c87524ecc4c81c2cad0e8649ded88577077
BKONTECH Team <110591367+BKONTECH@users.noreply.github.com>
2022-08-12 07:13:44 +0700

    Add more attributes to window open function in JS

--- func.js
+++ func.js
@@ -7,3 +7,15 @@ function myFunction() {
 function myFunction2() {
   window.open("https://www.bkontech.com", "_self");
 }
+
+function myFunction3() {
+  window.open("https://www.bkontech.com", "_blank");
+}
+
+function myFunction4() {
+  window.open("https://www.bkontech.com", "_parent");
+}
+  
+function myFunction5() {
+  window.open("https://www.bkontech.com", "_top");
+}
